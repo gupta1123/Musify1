d2ee48a91b715dec4816229a6fee234095c31769
gupta1123 <95712374+gupta1123@users.noreply.github.com>
2023-08-18 22:39:32 +0530

    Add files via upload

---
+++ musify (1)/src/components/RightSidebar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './RightSidebar.css';
+
+const RightSidebar = () => {
+  return (
+    <div className="card">
+      <div className="divider"></div> {/* Top Divider Line */}
+      
+      {/* Content will go here */}
+      <div className="divider"></div> {/* Bottom Divider Line */}
+      <p className="footer">Footer Text Here</p>
+    </div>
+  );
+}
+
+export default RightSidebar;
